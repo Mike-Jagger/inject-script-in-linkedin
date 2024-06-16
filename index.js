3da43b0c89315755e71a1f8d168be194046bf82c
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-15 21:54:19 -0600

    Changing to test files

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
-const COOKIES_PATH = 'cookies.json';
-const LOCAL_STORAGE_PATH = 'localStorage.json';
+const COOKIES_PATH = './auth/testCookies.json';
+const LOCAL_STORAGE_PATH = './auth/testLocalStorage.json';
 const LOGIN_PAGE = 'https://www.linkedin.com/login';
 
 async function loadCookiesAndLocalStorage(page) {
