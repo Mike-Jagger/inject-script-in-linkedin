d488dabb17c12919bdf0d857ce98356d9ec78c2b
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-17 00:40:01 -0600

    updating test script

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-console.log("Test");
+// Function to scroll down the page
+function continuousScroll() {
+    window.scrollBy(0, 10); // Scroll down by 10 pixels
+   }
+   
+// Set an interval to continuously call the scroll function
+setInterval(continuousScroll, 10); // Adjust the interval time for speed
