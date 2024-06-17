afc240df9b8a5c13b1b77ea839a995dd8b78a3df
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-17 00:40:15 -0600

    adjusting idents

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // Function to scroll down the page
 function continuousScroll() {
     window.scrollBy(0, 10); // Scroll down by 10 pixels
-   }
+}
    
 // Set an interval to continuously call the scroll function
 setInterval(continuousScroll, 10); // Adjust the interval time for speed
