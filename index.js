d0ee522bd46461f9c888f2919ed08e0bd4128ced
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-26 20:49:12 -0600

    Moving task to be performed on page to separate file

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const os = require('os');
+
 const path = require('path');
 const readline = require('readline');
 const schedule = require('node-schedule');
 const { sleep } = require('./sleep');
-const { clickButton } = require('./clickButton');
 const { ResourceManager } = require('./ResourceManager');
 
 const COOKIES_PATH = './auth/testCookies.json';
@@ -13,7 +12,6 @@ const SETTINGS_PATH = './testSettings.json';
 const LOCAL_STORAGE_PATH = './auth/testLocalStorage.json';
 const TXT_KEYWORDS = './keywords.txt';
 const JSON_KEYWORDS = './testKeywords.json';
-const TEST_SCRIPT = './test.js';
 const historyPath = './testHistory.json';
 const LOGIN_PAGE = 'https://www.linkedin.com/login';
 const LOCK_FILE = './lockfile';
@@ -219,44 +217,6 @@ async function moveToNextKeyword() {
     }
 }
 
-async function executeTestScriptInConsole(page, scriptPath) {
-    // Open the console
-    if (os.platform() === 'darwin') { // macOS
-        await page.keyboard.down('Meta');
-        await page.keyboard.press('Option');
-        await page.keyboard.press('J');
-        await page.keyboard.up('Meta');
-        await page.keyboard.up('Option');
-    } else if (os.platform() === 'win32') { // Windows
-        await page.keyboard.down('Control');
-        await page.keyboard.press('Shift');
-        await page.keyboard.press('J');
-        await page.keyboard.up('Control');
-        await page.keyboard.up('Shift');
-    }
-    console.log("Console opened");
-
-    // Wait for the console to open
-    await sleep(2000);
-
-    // Clear the console
-    await page.evaluate(() => {
-        console.clear();
-    });
-    console.log("Console cleared");
-
-
-    console.log("\n3. EXECUTE SCRIPT\n");
-
-    // Read the test script
-    const scriptContent = fs.readFileSync(scriptPath, 'utf-8');
-    console.log("Script loaded");
-
-    // Execute the script in the console
-    await page.evaluate(scriptContent);
-    console.log("Script executed");
-}
-
 // Define your ResourceManager instances
 let resourceManagers = [];
 
@@ -282,98 +242,12 @@ async function performAutomationTask(browserIndex, quadrant) {
 
     // Initialize ResourceManager if not already initialized
     if (!resourceManagers[browserIndex]) {
-        resourceManagers[browserIndex] = await new ResourceManager(settings, quadrant);
+        resourceManagers[browserIndex] = await new ResourceManager(settings, quadrant, currentKeyWord);
         await resourceManagers[browserIndex].init();
 
         const resourceManager = resourceManagers[browserIndex];
-        const page = await resourceManager.createPage();
-
-        await page.goto(LOGIN_PAGE);
-
-        sleep(3000);
-
-        await loadCookiesAndLocalStorage(page);
 
         resourceManager.browser.disconnect();
-
-        let pageErrCount = 3;
-
-        while(pageErrCount) {
-            try {
-                await page.goto('https://www.linkedin.com/');
-
-                try {
-                    await page.waitForSelector('aside[id="msg-overlay"]', { timeout: 60000 });
-                } catch (e) {
-                    console.error("No message overlay box found");
-                }
-                await sleep(2000);
-
-                await page.evaluate(() => {
-                    const msgOverlay = document.getElementById('msg-overlay');
-                    console.log(msgOverlay);
-                    if (msgOverlay) {
-                        msgOverlay.style.display = 'none';
-                    }
-                });
-
-                await page.waitForSelector('button[aria-label="Click to start a search"]', { timeout: 5000 });
-                try {
-                    await page.click('button[aria-label="Click to start a search"]');
-                } catch(e) {
-                    console.error("No search button found");
-                }
-                await page.type('input[aria-label="Search"]', currentKeyWord.keyword, { delay: 200 });
-                await page.keyboard.press('Enter');
-                await sleep(10000);
-
-                try {
-                    await page.waitForSelector('button[aria-label*="Show all filters"]', { timeout: 5000});
-                } catch(e) {
-                    console.error("There is no 'show all filters button'");
-                }
-
-                await clickButton(page, ['Posts']);
-
-                // OPEN AND CLEAR CONSOLE
-                console.log("\n3. OPEN AND CLEAR CONSOLE\n");
-                await executeTestScriptInConsole(page, TEST_SCRIPT);
-
-                // Monitor scrolling activity
-                let lastScrollTime = Date.now();
-
-                // Function to monitor the scrolling activity
-                const monitorScroll = async () => {
-                    while (true) {
-                        const newScrollPosition = await page.evaluate(() => window.scrollY);
-                        if (newScrollPosition > 0) {
-                            lastScrollTime = Date.now();
-                        }
-                        await sleep(10000); // Check every 10 seconds
-                        if (Date.now() - lastScrollTime > 10 * 60 * 1000) { // 10 minutes
-                            console.log("No scrolling detected for 10 minutes. Restarting the browser.");
-                            await resourceManager.release();
-                            resourceManagers[browserIndex] = null;
-                            await performAutomationTask(browserIndex, quadrant);
-                            break;
-                        }
-                    }
-                };
-
-                monitorScroll();
-
-                pageErrCount = 0;
-            } catch (e) {
-                console.log("Error while loading page:", e);
-                try {
-                    await page.goto(LOGIN_PAGE);
-                } catch(e) {
-                    console.error("Error redirecting to login page (most likely browser crash): ", e);
-                }
-                sleep(2000);
-                pageErrCount--;
-            }
-        }
 
         // Close browser after the specified duration
         console.log("\n4. MAKE CODE RUN FOR SPECIFIED HOURS\n");
