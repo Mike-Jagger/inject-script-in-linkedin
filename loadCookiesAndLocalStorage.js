2b3ae475df57b480b4599e90ffcb7ca0084febb7
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-28 23:06:24 -0600

    Adding try catch in case of unexpected close

--- loadCookiesAndLocalStorage.js
+++ loadCookiesAndLocalStorage.js
@@ -4,19 +4,23 @@ const LOCAL_STORAGE_PATH = './auth/testLocalStorage.json';
 const fs = require('fs');
 
 async function loadCookiesAndLocalStorage(page) {
-    if (fs.existsSync(COOKIES_PATH)) {
-        const cookies = JSON.parse(fs.readFileSync(COOKIES_PATH, 'utf-8'));
-        for (const cookie of cookies) {
-            await page.setCookie(cookie);
-        }
-    }
-    if (fs.existsSync(LOCAL_STORAGE_PATH)) {
-        const localStorageData = JSON.parse(fs.readFileSync(LOCAL_STORAGE_PATH, 'utf-8'));
-        await page.evaluate(data => {
-            for (const [key, value] of Object.entries(data)) {
-                localStorage.setItem(key, value);
+    try {
+        if (fs.existsSync(COOKIES_PATH)) {
+            const cookies = JSON.parse(fs.readFileSync(COOKIES_PATH, 'utf-8'));
+            for (const cookie of cookies) {
+                await page.setCookie(cookie);
             }
-        }, localStorageData);
+        }
+        if (fs.existsSync(LOCAL_STORAGE_PATH)) {
+            const localStorageData = JSON.parse(fs.readFileSync(LOCAL_STORAGE_PATH, 'utf-8'));
+            await page.evaluate(data => {
+                for (const [key, value] of Object.entries(data)) {
+                    localStorage.setItem(key, value);
+                }
+            }, localStorageData);
+        }
+    } catch(e) {
+        console.error("Error loading cookies to page: ", e);
     }
 }
 
