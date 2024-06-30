6f449da1cfbafadfdee38ceeae01cca1cbbf39c7
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-30 00:06:26 -0600

    Refactoring logging method

--- utils/logger.js
+++ utils/logger.js
@@ -1,40 +1,30 @@
-/**
- * Configurations of logger.
- */
 const winston = require('winston');
 const winstonRotator = require('winston-daily-rotate-file');
 
-const consoleConfig = [
-  new winston.transports.Console({
-    'colorize': true
-  })
-];
-
-const createLogger = new winston.Logger({
-  'transports': consoleConfig
+var logger = new winston.createLogger({
+  transports: [
+    new (winston.transports.DailyRotateFile)({
+      name: 'access-file',
+      level: 'info',
+      filename: './../logs/access.log',
+      json: false,
+      datePattern: 'yyyy-MM-DD',
+      prepend: true,
+      maxFiles: 10
+    }),
+    new (winston.transports.DailyRotateFile)({
+      name: 'error-file',
+      level: 'error',
+      filename: './../logs/error.log',
+      json: false,
+      datePattern: 'yyyy-MM-DD',
+      prepend: true,
+      maxFiles: 10
+    })
+  ]
 });
 
-const successLogger = createLogger;
-successLogger.add(winstonRotator, {
-  'name': 'access-file',
-  'level': 'info',
-  'filename': './../logs/access.log',
-  'json': false,
-  'datePattern': 'yyyy-MM-dd-',
-  'prepend': true
-});
-
-const errorLogger = createLogger;
-errorLogger.add(winstonRotator, {
-  'name': 'error-file',
-  'level': 'error',
-  'filename': './../logs/error.log',
-  'json': false,
-  'datePattern': 'yyyy-MM-dd-',
-  'prepend': true
-});
 
 module.exports = {
-  'successlog': successLogger,
-  'errorlog': errorLogger
+  logger
 };
